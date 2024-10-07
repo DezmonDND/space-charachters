8a9b0a7536996b783b55fecd9d85784b397379ad
unknown <denedoseikin@mail.ru>
2024-10-08 00:53:03 +0700

    fix: исправить адаптив

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Main from "./components/Main/Main";
 
 function App() {
   return (
-    <div className="flex items-center justify-center text-center bg-black text-white h-screen">
+    <div className="flex items-center justify-center text-center bg-black text-white h-screen customvh:items-baseline overflow-auto">
       <Main></Main>
     </div>
   );
